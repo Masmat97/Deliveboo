683560710590fe10df4ade7a1edbce7a7c8d958d
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-08-30 15:23:27 +0200

    Add newType in TypeSeeder and migration upload DatabaseSeeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,100 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $newType = new Type();
+        $newType->name = 'Italian';
+        $newType->save();
+
+        $newType1 = new Type();
+        $newType1->name = 'Japanese';
+        $newType1->save();
+
+        $newType2 = new Type();
+        $newType2->name = 'Indian';
+        $newType2->save();
+
+        $newType3 = new Type();
+        $newType3->name = 'Seafood';
+        $newType3->save();
+
+        $newType4 = new Type();
+        $newType4->name = 'American';
+        $newType4->save();
+
+        $newType5 = new Type();
+        $newType5->name = 'Kebab';
+        $newType5->save();
+
+        $newType6 = new Type();
+        $newType6->name = 'Middle Eastern';
+        $newType6->save();
+
+        $newType7 = new Type();
+        $newType7->name = 'Hamburger';
+        $newType7->save();
+
+        $newType8 = new Type();
+        $newType8->name = 'Chinese';
+        $newType8->save();
+
+        $newType9 = new Type();
+        $newType9->name = 'Mexican';
+        $newType9->save();
+
+        $newType10 = new Type();
+        $newType10->name = 'French';
+        $newType10->save();
+
+        $newType11 = new Type();
+        $newType11->name = 'Greek';
+        $newType11->save();
+
+        $newType12 = new Type();
+        $newType12->name = 'Thai';
+        $newType12->save();
+
+        $newType13 = new Type();
+        $newType13->name = 'Vietnamese';
+        $newType13->save();
+
+        $newType14 = new Type();
+        $newType14->name = 'Korean';
+        $newType14->save();
+
+        $newType15 = new Type();
+        $newType15->name = 'Spanish';
+        $newType15->save();
+
+        $newType16 = new Type();
+        $newType16->name = 'German';
+        $newType16->save();
+
+        $newType17 = new Type();
+        $newType17->name = 'Brazilian';
+        $newType17->save();
+
+        $newType18 = new Type();
+        $newType18->name = 'Argentine';
+        $newType18->save();
+
+        $newType19 = new Type();
+        $newType19->name = 'Peruvian';
+        $newType19->save();
+
+        $newType20 = new Type();
+        $newType20->name = 'Fusion';
+        $newType20->save();
+
+        $newType21 = new Type();
+        $newType21->name = 'Fast-food';
+        $newType21->save();
+
+        $newType22 = new Type();
+        $newType22->name = 'Healty';
+        $newType22->save();
+
+        $newType23 = new Type();
+        $newType23->name = 'Pokè';
+        $newType23->save();
     }
 }
