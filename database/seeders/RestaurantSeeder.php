db23ab4b0285946b796ede4547c1c2216049fbcd
Masmat97 <matteo.mascio@libero.it>
2024-08-30 15:17:12 +0200

    add real image to restaurant seeder

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -17,7 +17,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant2->name = 'La Bella Vita';
         $newRestaurant2->address = 'Via del Corso, 18, 00187 Roma RM, Italy';
         $newRestaurant2->p_iva = 'IT1234567890';
-        $newRestaurant2->image = 'https://example.com/la-bella-vita.jpg';
+        $newRestaurant2->image = 'https://img1.wsimg.com/isteam/ip/587e2db3-e147-4a44-9766-3c9c8d5285ca/blob-0001.png';
         $newRestaurant2->user_id = 2;
         $newRestaurant2->save();
 
@@ -25,7 +25,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant3->name = 'Il Pagliaccio';
         $newRestaurant3->address = 'Via dei Banchi Vecchi, 129/a, 00187 Roma RM, Italy';
         $newRestaurant3->p_iva = 'IT9876543210';
-        $newRestaurant3->image = 'https://example.com/il-pagliaccio.jpg';
+        $newRestaurant3->image = 'https://www.thegastrojob.com/wp-content/uploads/2018/02/Ristorante-il-Pagliaccio-a-Roma.jpg';
         $newRestaurant3->user_id = 3;
         $newRestaurant3->save();
         // Michelin-starred Italian restaurant
@@ -34,7 +34,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant4->name = 'Sushi Ko';
         $newRestaurant4->address = 'Via della Lungaretta, 21, 00153 Roma RM, Italy';
         $newRestaurant4->p_iva = 'IT1111111111';
-        $newRestaurant4->image = 'https://example.com/sushi-ko.jpg';
+        $newRestaurant4->image = 'https://www.zenit-group.it/wp-content/uploads/2016/02/Sushiko-Ferrara-foto-10-500x500.jpg';
         $newRestaurant4->user_id = 4;
         $newRestaurant4->save();
 
@@ -44,7 +44,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant5->name = 'Tandoori Nights';
         $newRestaurant5->address = 'Via dei Serpenti, 124, 00187 Roma RM, Italy';
         $newRestaurant5->p_iva = 'IT2222222222';
-        $newRestaurant5->image = 'https://example.com/tandoori-nights.jpg';
+        $newRestaurant5->image = 'https://photos.wikimapia.org/p/00/03/68/93/41_full.jpg';
         $newRestaurant5->user_id = 5;
         $newRestaurant5->save();
         // Indian restaurant
@@ -53,7 +53,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant6->name = 'La Rosetta';
         $newRestaurant6->address = 'Piazza del Popolo, 18, 00187 Roma RM, Italy';
         $newRestaurant6->p_iva = 'IT3333333333';
-        $newRestaurant6->image = 'https://example.com/la-rosetta.jpg';
+        $newRestaurant6->image = 'https://www.sercolombiano.com/wp-content/uploads/2018/02/DSC_0009.jpg';
         $newRestaurant6->user_id = 6;
         $newRestaurant6->save();
         // Seafood restaurant
@@ -62,7 +62,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant7->name = 'Burger Bar';
         $newRestaurant7->address = 'Via del Corso, 241, 00187 Roma RM, Italy';
         $newRestaurant7->p_iva = 'IT4444444444';
-        $newRestaurant7->image = 'https://example.com/burger-bar.jpg';
+        $newRestaurant7->image = 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/roma-burger-bar-glendalina-ziemba.jpg';
         $newRestaurant7->user_id = 7;
         $newRestaurant7->save();
         // American-style burger joint
@@ -71,7 +71,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant8->name = 'La Gensola';
         $newRestaurant8->address = 'Piazza della Gensola, 15, 00153 Roma RM, Italy';
         $newRestaurant8->p_iva = 'IT5555555555';
-        $newRestaurant8->image = 'https://example.com/la-gensola.jpg';
+        $newRestaurant8->image = 'https://media-cdn.tripadvisor.com/media/photo-s/16/ed/8f/99/photo0jpg.jpg';
         $newRestaurant8->user_id = 8;
         $newRestaurant8->save();
         // Traditional Roman trattoria
@@ -80,7 +80,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant9->name = 'Kebabish';
         $newRestaurant9->address = 'Via dei Mille, 15, 00187 Roma RM, Italy';
         $newRestaurant9->p_iva = 'IT6666666666';
-        $newRestaurant9->image = 'https://example.com/kebabish.jpg';
+        $newRestaurant9->image = 'https://media-cdn.tripadvisor.com/media/photo-s/11/98/81/f7/kebabish-curry-and-grill.jpg';
         $newRestaurant9->user_id = 9;
         $newRestaurant9->save();
         // Middle Eastern kebab shop
@@ -89,7 +89,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant10->name = 'La Campana';
         $newRestaurant10->address = 'Via Campana, 47, 00187 Roma RM, Italy';
         $newRestaurant10->p_iva = 'IT7777777777';
-        $newRestaurant10->image = 'https://example.com/la-campana.jpg';
+        $newRestaurant10->image = 'https://italianliving.com/custom/domain_1/image_files/sitemgr_photo_23534.jpg';
         $newRestaurant10->user_id = 10;
         $newRestaurant10->save();
         // Neapolitan-style pizzeria
@@ -98,7 +98,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant11->name = 'Sakura';
         $newRestaurant11->address = 'Via dei Giubbonari, 21, 00186 Roma RM, Italy';
         $newRestaurant11->p_iva = 'IT8888888888';
-        $newRestaurant11->image = 'https://example.com/sakura.jpg';
+        $newRestaurant11->image = 'https://is2-ssl.mzstatic.com/image/thumb/Purple115/v4/55/3b/d9/553bd9ae-784e-dc83-8700-044fb2da59d5/source/512x512bb.jpg';
         $newRestaurant11->user_id = 11;
         $newRestaurant11->save();
         // Japanese ramen restaurant
@@ -107,7 +107,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant12->name = 'La Carbonara';
         $newRestaurant12->address = 'Via Panonica, 10, 00187 Roma RM, Italy';
         $newRestaurant12->p_iva = 'IT9999999999';
-        $newRestaurant12->image = 'https://example.com/la-carbonara.jpg';
+        $newRestaurant12->image = 'https://media-cdn.tripadvisor.com/media/photo-s/10/e3/07/90/photo2jpg.jpg';
         $newRestaurant12->user_id = 12;
         $newRestaurant12->save();
         // Italian restaurant specializing in carbonara
@@ -116,7 +116,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant13->name = 'Tacos el Fogoncito';
         $newRestaurant13->address = 'Via dei Coronari, 135, 00186 Roma RM, Italy';
         $newRestaurant13->p_iva = 'IT1010101010';
-        $newRestaurant13->image = 'https://example.com/tacos-el-fogoncito.jpg';
+        $newRestaurant13->image = 'https://live.staticflickr.com/2256/2325273112_5e296f04dd.jpg';
         $newRestaurant13->user_id = 13;
         $newRestaurant13->save();
         // Mexican taqueria
@@ -125,7 +125,7 @@ class RestaurantSeeder extends Seeder
         $newRestaurant14->name = 'La Galette';
         $newRestaurant14->address = 'Via della Scrofa, 65, 00187 Roma RM, Italy';
         $newRestaurant14->p_iva = 'IT1212121212';
-        $newRestaurant14->image = 'https://example.com/la-galette.jpg';
+        $newRestaurant14->image = 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/99/09/0d/la-galetta.jpg?w=1200&h=-1&s=1';
         $newRestaurant14->user_id = 14;
         $newRestaurant14->save();
         // French bistro
