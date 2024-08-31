99fda25242fe56af837a820d96ed7ea58183ea4c
Antonino <drakon9927@libero.it>
2024-08-31 22:34:27 +0200

    Migliorie Index.blade

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,49 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12 d-flex flex-wrap">
-                @foreach ($restaurants as $restaurant)
-                    <div class="card my-4 mx-2" style="width: 18rem;">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
-                                <p class="card-text">
-                                <h5 class="fw-bold card-title">{{ $restaurant->name }}</h5>
-                                <a href="{{ route('admin.restaurants.show', $restaurant->id) }}">
-
-                                    @if (Str::startsWith($restaurant->image, 'http'))
-                                        <img src="{{ $restaurant->image }}" alt="">
-                                    @else
-                                        <img src="{{ asset('storage/' . $restaurant->image) }}" alt="">
-                                    @endif
-                                </a>
-                                </p>
-                                <div>
-                                    <p>Tipologie:</p>
-                                    @foreach ($restaurant->types as $type)
-                                        <p>{{ $type->name }}</p>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="my-3 d-flex flex-column">
-                            <button type="button" class="btn btn-primary"><a
-                                    href="{{ route('admin.restaurants.edit', $restaurant->id) }}">Modifica</a>
-                            </button>
-
-                            <form class="my-1" action="{{ route('admin.restaurants.destroy', $restaurant) }}"
-                                method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-primary  w-100">Elimina</button>
-                            </form>
-
-                        </div>
-
+<div class="container">
+    <h1 class="my-4">Elenco Ristoranti</h1>
+    <div class="row">
+        @foreach ($restaurants as $restaurant)
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="{{ $restaurant->image ? asset('storage/' . $restaurant->image) : asset('storage/placeholder.jpg') }}" alt="{{ $restaurant->name }}" style="width:100%; height:auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $restaurant->name }}</h5>
+                        <p class="card-text">
+                            <strong>Indirizzo:</strong> {{ $restaurant->address }}<br>
+                            <strong>Tipologie:</strong>
+                            @foreach ($restaurant->types as $type)
+                                <span class="badge bg-secondary">{{ $type->name }}</span>
+                            @endforeach
+                        </p>
+                        <a href="{{ route('admin.restaurants.show', $restaurant->id) }}" class="btn btn-primary">Dettagli</a>
+                        <a href="{{ route('admin.restaurants.edit', $restaurant->id) }}" class="btn btn-warning">Modifica</a>
+                        <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST" style="display: inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('Sei sicuro di voler eliminare questo ristorante?');">Elimina</button>
+                        </form>
                     </div>
-                @endforeach
+                </div>
             </div>
-        </div>
-    @endsection
+        @endforeach
+    </div>
+</div>
+@endsection
