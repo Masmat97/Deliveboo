8cbf48b6b52cd23150bca0a035233fd81aa99235
Antonino <drakon9927@libero.it>
2024-08-31 22:08:47 +0200

    Models aggiornati con relazioni

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Dish extends Model
 {
     use HasFactory;
+
+    // Relazione con Restaurant
+    public function restaurant() {
+        return $this->belongsTo(Restaurant::class);
+    }
 }
