40858bddfe866eebdb483084aa1c8fe64641d5c7
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-09-02 15:30:21 +0200

    new page restaurants

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -3,37 +3,32 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 d-flex flex-wrap">
+            <div class="col-12">
                 @foreach ($restaurants as $restaurant)
-                    <div class="card my-4 mx-2" style="width: 18rem;">
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <div>
-                                <p class="card-text">
-                                <h5 class="fw-bold card-title">{{ $restaurant->name }}</h5>
-                                <a href="{{ route('admin.restaurants.show', $restaurant->id) }}">
+                    <h5 class="mt-3 fw-bold card-title">{{ $restaurant->name }}</h5>
+                    <div class="mt-3 mb-3">
+                        <a href="{{ route('admin.dishes.index') }}">
 
-                                    @if (Str::startsWith($restaurant->image, 'http'))
-                                        <img src="{{ $restaurant->image }}" alt="">
-                                    @else
-                                        <img src="{{ asset('storage/' . $restaurant->image) }}" alt="">
-                                    @endif
-                                </a>
-                                </p>
-                                <div>
-                                    <p>Tipologie:</p>
-                                    @foreach ($restaurant->types as $type)
-                                        <p>{{ $type->name }}</p>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
+                            @if (Str::startsWith($restaurant->image, 'http'))
+                                <img src="{{ $restaurant->image }}" alt="">
+                            @else
+                                <img src="{{ asset('storage/' . $restaurant->image) }}" alt="">
+                            @endif
+                        </a>
+                    </div>
+                    <div>
+                        <span>Tipologia di ristorante:</span>
+                        @foreach ($restaurant->types as $type)
+                            <span>{{ $type->name }}</span>
+                        @endforeach
+                    </div>
 
-                        <div class="my-3 d-flex flex-column">
-                            <button type="button" class="btn btn-primary"><a
-                                    href="{{ route('admin.dishes.index') }}">Visualizza Menù</a>
-                            </button>
-                        </div>
 
+
+                    <div class="my-3 d-flex flex-column">
+                        <button type="button" class="btn btn-primary"><a
+                                href="{{ route('admin.dishes.index') }}">Visualizza Menù</a>
+                        </button>
                     </div>
                 @endforeach
             </div>
