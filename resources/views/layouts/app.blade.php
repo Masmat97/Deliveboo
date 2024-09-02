dbb89030b07342fbb9e895d891033f635afb1e28
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-09-02 09:50:40 +0200

    add restaurant link in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.restaurants.index') }}">{{ __('Restaurant') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
