f618b3bb00bffbec9b11c0b4f7057c5012646c94
Antonino <drakon9927@libero.it>
2024-09-04 15:53:46 +0200

    vite importato e componente carrello

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,19 @@
+import { createApp } from 'vue';
 import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
-])
+]);
+
+// Importa il componente Vue
+import CartComponent from './components/CartComponent.vue';
+
+// Crea l'app Vue e monta il componente
+const app = createApp({
+    components: {
+        'cart-component': CartComponent,
+    }
+});
+
+app.mount('#app');
