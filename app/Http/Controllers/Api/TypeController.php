d665bf75949ef874e7bb62c62eda33b26b434835
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-09-05 11:16:42 +0200

    Modify api types

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,31 @@ class TypeController extends Controller
         return response()->json([
             'success' => true,
             'types' => Type::all()
+        ]);
+    }
+
+    public function show(Request $request)
+    {
+        $types = explode(',', $request->input('types')); // Convert string to array
+
+        if (!$types) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No types provided'
+            ], 400);
+        }
+
+        $restaurants = Restaurant::where(function ($query) use ($types) {
+            foreach ($types as $type) {
+                $query->whereHas('types', function ($subQuery) use ($type) {
+                    $subQuery->where('name', $type);
+                });
+            }
+        })->get();
+
+        return response()->json([
+            'success' => true,
+            'restaurants' => $restaurants
         ]);
     }
 }
