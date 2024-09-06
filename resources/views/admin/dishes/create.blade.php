a997e994b9006cc91f56dfa4b956e9bd47ba4ed5
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-09-06 13:06:47 +0200

    validate_dish_create.js  and modify file create.blade

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -5,24 +5,22 @@
     <div class="form-group p-3">
         <label for="name" class="form-label p-2"><b>Name:*</b></label>
         <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
-        @error('name')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="form-group p-3">
-        <label for="image" class="form-label p-2"><b>Image:*</b></label>
-        <input type="file" class="form-control" id="image" name="image">
-        @error('image')
+        <div id="name-error"></div>
+      </div>
+<div class="form-group p-3">
+    <label for="image" class="form-label p-2"><b>Image:*</b></label>
+    <input type="file" class="form-control" id="image" name="image">
+
+    <div id="error-message"></div>
+</div>
+<div class="form-group p-3">
+    <label for="ingredient" class="form-label p-2"><b>Ingredient:*</b></label>
+    <textarea class="form-control" id="ingredient" name="ingredient">{{ old('ingredient') }}</textarea>
+    <p id="message"></p>
+    @error('ingredient')
         <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-    <div class="form-group p-3">
-        <label for="ingredient" class="form-label p-2"><b>Ingredient:*</b></label>
-        <textarea class="form-control" id="ingredient" name="ingredient">{{ old('ingredient') }}</textarea>
-        @error('ingredient')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
+    @enderror
+</div>
     <div class="form-group p-3">
         <label for="price" class="form-label p-2"><b>Price:*</b></label>
         <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}">
@@ -44,4 +42,5 @@
         obbligatori </div>
     <button type="submit" class="btn btn-primary m-3">Add</button>
 </form>
+<script src="{{ asset('js/validate_dish_create.js') }}"></script>
 @endsection
