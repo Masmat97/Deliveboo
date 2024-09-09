e21026b1819b9426a8cb51b96cfe67ca43d801df
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-09-09 17:08:38 +0200

    responsive-sm

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h1 class="text-center p-4">Menù</h1>
         <div class="table-responsive p-4">
-            <table class="table table-striped table-bordered">
+            <table class="table table-sm table-striped table-bordered">
                 <thead>
                     <tr>
                         <th>Nome</th>
@@ -15,25 +15,66 @@
                 </thead>
                 <tbody>
                     @foreach ($dishes as $dish)
-                        <tr>
-                            <td>{{ $dish->name }}</td>
-                            <td>
-                                @if (Str::startsWith($dish->image, 'http'))
-                                    <img src="{{ $dish->image }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
-                                @else
-                                    <img src="{{ asset('storage/' . $dish->image) }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
-                                @endif
-                            </td>
-                            <td>{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e(Str::limit($dish->ingredient, 100, ' [Read more]')))) !!}</td>
-                                <td>
-                                    <div class="d-flex justify-content-around">
-                                        <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary btn-sm m-1">Dettagli</a>
-                                    </div>
-                                </td>
-                        </tr>
-                    @endforeach
+    <tr>
+        <td data-label="Nome">{{ $dish->name }}</td>
+        <td data-label="Immagini">
+            @if (Str::startsWith($dish->image, 'http'))
+                <img src="{{ $dish->image }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
+            @else
+                <img src="{{ asset('storage/' . $dish->image) }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
+            @endif
+        </td>
+        <td data-label="Ingredienti">{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e(Str::limit($dish->ingredient, 100, ' [Read more]')))) !!}</td>
+        <td data-label="Azioni">
+            <div class="d-flex justify-content-around">
+                <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary btn-sm m-1">Dettagli</a>
+            </div>
+        </td>
+    </tr>
+@endforeach
                 </tbody>
             </table>
         </div>
     </div>
 @endsection
+
+<style scoped>
+    @media (max-width: 425px) {
+     .table-sm {
+         display: block;
+         width: 100%;
+     }
+     .table-sm thead {
+         display: none;
+     }
+     .table-sm tr {
+         display: block;
+         margin-bottom: 20px;
+         border-bottom: 2px solid #ccc;
+     }
+     .table-sm td {
+         display: block;
+         width: 100%;
+         padding: 10px;
+         border: none;
+         border-bottom: 1px solid #ccc;
+     }
+     .table-sm td:before {
+         content: attr(data-label);
+         font-weight: bold;
+         margin-right: 10px;
+     }
+     .table-sm tr:not(:last-child) {
+         margin-bottom: 30px;
+     }
+     .table-sm td img {
+         width: 50%;
+         height: auto;
+         margin: 10px auto;
+         display: block;
+     }
+     .table-sm td p {
+         margin-bottom: 10px;
+     }
+ }
+ </style>
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h1 class="text-center p-4">{{ $dish->name }}</h1>
         <div class="table-responsive p-4">
-            <table class="table table-striped table-bordered">
+            <table class="table table-sm table-striped table-bordered">
                 <thead>
                     <tr>
                         <th>Nome</th>
@@ -15,28 +15,78 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td>{{ $dish->name }}</td>
-                        <td>
+                        <td data-label="Nome">{{ $dish->name }}</td>
+                        <td data-label="Immagini">
                             @if (Str::startsWith($dish->image, 'http'))
                                 <img src="{{ $dish->image }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
                             @else
                                 <img src="{{ asset('storage/' . $dish->image) }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
                             @endif
                         </td>
-                        <td>{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e($dish->ingredient))) !!}</td>
-                            <td>
-                                <div class="d-flex flex-column">
-                                    <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-warning btn-sm m-1">Modifica</a>
-                                    <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Sei sicuro di eliminare questo piatto?')">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm m-1">Elimina</button>
-                                    </form>
-                                </div>
-                            </td>
+                        <td data-label="Ingredienti">{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e($dish->ingredient))) !!}</td>
+                        <td data-label="Azioni" class="d-flex justify-content-around">
+                            <form action="{{ route('admin.dishes.edit', $dish->id) }}" method="GET" class="d-inline">
+                                @csrf
+                                <button type="submit" class="btn btn-warning btn-sm">Modifica</button>
+                            </form>
+                            <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Sei sicuro di eliminare questo piatto?')">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
+                            </form>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
 @endsection
+
+<style scoped>
+    @media (max-width: 425px) {
+        .table-sm {
+            display: block;
+            width: 100%;
+        }
+        .table-sm thead {
+            display: none;
+        }
+        .table-sm tr {
+            display: block;
+            margin-bottom: 20px;
+            border-bottom: 2px solid #ccc;
+        }
+        .table-sm td {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-bottom: 1px solid #ccc;
+        }
+        .table-sm td:before {
+            content: attr(data-label);
+            font-weight: bold;
+            margin-right: 10px;
+        }
+        .table-sm tr:not(:last-child) {
+            margin-bottom: 30px;
+        }
+        .table-sm td img {
+            width: 50%;
+            height: auto;
+            margin: 10px auto;
+            display: block;
+        }
+    }
+
+    @media (max-width: 992px) and (min-width: 425px) {
+    .table-sm td:last-child {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .table-sm td:last-child form {
+        margin-bottom: 10px;
+    }
+}
+</style>
