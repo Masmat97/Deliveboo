4c8a99bbb8b2d9a54ce3d0502028773b3e8b0d5a
Antonino <drakon9927@libero.it>
2024-09-10 11:42:00 +0200

    .

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,20 +10,20 @@ class CartController extends Controller
 {
     public function index()
     {
-        // Recupera gli elementi del carrello dalla sessione o inizializza un array vuoto
         $cart = Session::get('cart', []);
-
-        // Calcola il totale del carrello
         $total = array_reduce($cart, function ($carry, $item) {
             return $carry + ($item['price'] * $item['quantity']);
         }, 0);
-
-        // Ritorna la vista del carrello con gli elementi e il totale
+    
+        // Aggiungi questo per il debug
+        dd($cart, $total); // Visualizza i dati del carrello e il totale
+    
         return view('cart.index', [
             'cart' => $cart,
             'total' => $total
         ]);
     }
+    
 
     public function add(Request $request, Dish $dish)
     {
