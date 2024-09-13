11ae4a56242def36566169843442fa97184a86d5
Masmat97 <matteo.mascio@libero.it>
2024-09-13 15:03:37 +0200

    modifiche dishes

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,16 +7,18 @@
             <table class="table table-sm table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th>Nome</th>
+                       
                         <th>Immagini</th>
-                        <th>Ingredienti</th>
+                        <th>Nome</th>
+                        {{-- <th>Ingredienti</th> --}}
+                        <th>Prezzo</th>
                         <th>Azioni</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($dishes as $dish)
     <tr>
-        <td data-label="Nome">{{ $dish->name }}</td>
+        
         <td data-label="Immagini">
             @if (Str::startsWith($dish->image, 'http'))
                 <img src="{{ $dish->image }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
@@ -24,10 +26,20 @@
                 <img src="{{ asset('storage/' . $dish->image) }}" alt="" class="img-fluid" style="width: 100px; height: 100px; object-fit: cover;">
             @endif
         </td>
-        <td data-label="Ingredienti">{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e(Str::limit($dish->ingredient, 100, ' [Read more]')))) !!}</td>
+        <td data-label="Nome">{{ $dish->name }}</td>
+        <td data-label="Nome">{{ $dish->price }}</td>
+        {{-- <td data-label="Ingredienti">{!! preg_replace('/\n{2,}/', '</p><p>', nl2br(e(Str::limit($dish->ingredient, 100, ' [Read more]')))) !!}</td> --}}
         <td data-label="Azioni">
             <div class="d-flex justify-content-around">
-                <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary btn-sm m-1">Dettagli</a>
+                {{-- <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary btn-sm m-1">Dettagli</a> --}}
+                <form action="{{ route('admin.dishes.edit', $dish->id) }}" method="GET" class="d-inline">
+                    @csrf
+                    <button type="submit" class="btn btn-warning btn-sm">Modifica</button>
+                </form>
+                <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Sei sicuro di eliminare questo piatto?')">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
             </div>
         </td>
     </tr>
