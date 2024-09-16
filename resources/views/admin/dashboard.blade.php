dbb17a649be1cc1797665881124f67eb354774a7
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-09-16 11:10:09 +0200

    Add btn "vai al menu"

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,45 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+<!DOCTYPE html>
+<html>
 
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <style>
+        .fade-in {
+            opacity: 0;
+            transition: opacity 4s;
+        }
+
+        .fade-in.loaded {
+            opacity: 1;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron text-center">
+            <h1 id="welcome-text" class="display-1 fade-in mt-5">Benvenuto!</h1>
+        </div>
+
+        <div class="btn-container d-flex justify-content-center">
+            <a class="btn btn-primary mt-5 text-center" href="{{ route('admin.dishes.index') }}">{{ __('Vai al Menù') }}</a>
         </div>
     </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            document.getElementById("welcome-text").classList.add("loaded");
+        });
+    </script>
+</body>
+
+</html>
+
+
 @endsection
