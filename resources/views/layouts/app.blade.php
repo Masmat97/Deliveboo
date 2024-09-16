443dec8d6d693d76ef335eb6dbe8b45567467b51
Antonino <drakon9927@libero.it>
2024-09-16 11:22:34 +0200

    script chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 
 				<meta name="csrf-token" content="{{ csrf_token() }}">
-
+				<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 				<title>Deliveboo</title>
 
 				<!-- Fonts -->
