8f8c8e2578e21629422642dd3f08cb2cb69bb123
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2025-02-03 14:41:27 +0100

    Problemi passaggio ordine da front a back

--- app/Http/Controllers/Api/BraintreeController.php
+++ app/Http/Controllers/Api/BraintreeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Ause Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Braintree\Gateway;
 
